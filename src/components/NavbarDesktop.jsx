6b45311b3d3435f1850274cb032e9701da42c0a1
Nnamdi James <egentinnamdi10@gmail.com>
2024-07-10 08:11:38 +0100

    6.0

--- src/components/NavbarDesktop.jsx
+++ src/components/NavbarDesktop.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function NavbarDesktop() {
   return (
     <div className="flex  justify-around items-center capitalize text-2xl font-semibold text-primary">
-      <span>home</span>
+      <NavLink to="/home">home</NavLink>
       <NavLink to="/cart">store</NavLink>
       <span>contact</span>
       <span>label</span>
--- src/components/StoreDesktop.jsx
+++ src/components/StoreDesktop.jsx
@@ -1,23 +1,60 @@
+import Logo from "./Logo";
+import NavbarDesktop from "./NavbarDesktop";
+
 function StoreDesktop() {
   return (
-    <div className="bg-secondaryTwo  min-h-screen  p-10 grid grid-cols-3 grid-rows-2">
-      <div>
-        <img data-src="" alt="" className="lazyload" data-sizes="auto" />
-      </div>
-      <div>
-        <img data-src="" alt="" className="lazyload" data-sizes="auto" />
-      </div>
-      <div>
-        <img data-src="" alt="" className="lazyload" data-sizes="auto" />
-      </div>
-      <div>
-        <img data-src="" alt="" className="lazyload" data-sizes="auto" />
-      </div>
-      <div>
-        <img data-src="" alt="" className="lazyload" data-sizes="auto" />
-      </div>
-      <div>
-        <img data-src="" alt="" className="lazyload" data-sizes="auto" />
+    <div className="grid grid-cols-[1.5fr_2fr] grid-rows-[1fr_4fr] bg-secondaryTwo">
+      <Logo />
+      <NavbarDesktop />
+      <div className="bg-secondaryTwo  min-h-screen  p-10 grid grid-cols-3 grid-rows-2 col-span-2 gap-10">
+        <div className="bg-white p-5 rounded-2xl grid place-items-center">
+          <img
+            data-src="/shirt-blue.png"
+            alt=""
+            className="lazyload"
+            data-sizes="auto"
+          />
+        </div>
+        <div className="bg-white p-5 rounded-2xl grid place-items-center">
+          <img
+            data-src="/bag.png"
+            alt=""
+            className="lazyload"
+            data-sizes="auto"
+          />
+        </div>
+        <div className="bg-white p-5 rounded-2xl grid place-items-center">
+          <img
+            data-src="/glasses.png"
+            alt=""
+            className="lazyload"
+            data-sizes="auto"
+          />
+        </div>
+        <div className="bg-white p-5 rounded-2xl grid place-items-center">
+          <img
+            data-src="/shirt-brown.png"
+            alt=""
+            className="lazyload"
+            data-sizes="auto"
+          />
+        </div>
+        <div className="bg-white p-5 rounded-2xl grid place-items-center">
+          <img
+            data-src="/shirt.png"
+            alt=""
+            className="lazyload"
+            data-sizes="auto"
+          />
+        </div>
+        <div className="bg-white p-5 rounded-2xl grid place-items-center">
+          <img
+            data-src="/shoe.png"
+            alt=""
+            className="lazyload"
+            data-sizes="auto"
+          />
+        </div>
       </div>
     </div>
   );
